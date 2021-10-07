7203ae0cfa25c400373efade56adb519382f3667
Marco Happe <ITN247448@qualifizierung.at>
2021-10-07 09:43:49 +0200

    Add project files.

---
+++ EF_01_Grundlagen/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_01_Grundlagen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Alle Datensätze der Tabelle "Products" anzeigen
+            // -----------------------------------------------
+
+            // Als erstes ein Contextobjekt erstellen (in Model1.Context.cs zu finden)
+            // Zur using Anweisung gehört ein Codeblock, darum die {}
+            using (NorthwindEntities context = new NorthwindEntities()) 
+            { 
+
+            }
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
