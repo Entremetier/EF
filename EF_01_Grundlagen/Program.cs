56d3a2d07dcd02f0558d725c8ec7c6d6c25dc56b
Marco Happe <ITN247448@qualifizierung.at>
2021-10-19 13:09:23 +0200

    add new project 02

--- EF_01_Grundlagen/Program.cs
+++ EF_01_Grundlagen/Program.cs
@@ -4,6 +4,71 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Entity Framework (EF)
+ * 
+ * Mit Hilfe des EFs kann man auf einfache Weise aus einer .NET Applikation auf
+ * relationale Datenbanken zugreifen.
+ * 
+ * Das EF überbrückt den Unterschied zwischen den Datenstrukturen in der Programmierung
+ * und der persistenten (dauerhaften) Datenspeicherung in relationalen Datenbanken.
+ * Objektorientierte Programmiersprachen wie C# arbeiten mit Klassen und Objekten,
+ * Datenbanken mit Tabellen.
+ * Ein objekt-relationaler Mapper (O/R-Mapper) bildet Daten aus einer relationalen
+ * Datenbank auf ein objektorientiertes Datenmodell ab, das bei der Programmierung
+ * verwendet wird. Tabellen werden auf Klassen abgebildet, Spalten auf Eigenschaften,
+ * Datensätze auf Objekte und Fremdschlüsselbeziehungen auf Objektreferenzen.
+ * 
+ * Der Programmierer muss sich nicht selbst um die Transformation zwischen Datensätzen
+ * und Objekten kümmern. Er kann das EF anweisen, Daten aus der Datenbank zu laden bzw.
+ * dort zu speichern. Dabei übernimmt das EF auch das Erstellen der INSERT-, UPDATE-
+ * und DELETE-Anweisungen.
+ * 
+ * Das EF bildet Datenbanken auf objektorientierte Konzepte entsprechend dem sog.
+ * Entity Data Model (EDM) ab. Dieses besteht aus den folgenden 3 Schichten:
+ *    1) Konzeptionelle Schicht (Conceptual Layer)
+ *    2) Logische Schicht (Logical Layer) und
+ *    3) Zuordnungsschicht (Mapping Layer)
+ * 
+ * Die konzeptionelle Schicht beschreibt das zu verwendende Objektmodell, bestehend aus
+ * Klassen, den sog. Entitäten oder Entitätsklassen, die miteinander in Beziehung
+ * stehen (können). Die logische Schicht beschreibt das Schema der Datenbank.
+ * Dazwischen liegt die Zuordnungsschicht, die die Elemente der konzeptionellen Schicht
+ * auf die Elemente der logischen Schicht abbildet. Beispielsweise beschreibt sie, dass
+ * eine Klasse Person auf eine Tabelle Person abgebildet wird, dass eine Eigenschaft Id
+ * einer Spalte PersonId entspricht oder dass ein bestimmter Fremdschlüssel zum
+ * Auflösen einer Objektbeziehung heranzuziehen ist.
+ * 
+ * In Visual Studio hat der Entwickler im Rahmen des EF's 3 Entwurfsmöglichkeiten:
+ *    1) Database First
+ *    2) Model First und
+ *    3) Code First
+ * Wir werden ausschließlich Database First anwenden. Bei dieser Entwurfsart wird das
+ * EDM automatisch aus den Objekten einer bestehenden Datenbank erstellt.
+ * 
+ * Neben den einzelnen Entitätsklassen hat der Entwickler auch Zugriff auf den sog.
+ * Datenbankkontext oder kurz Kontext. Der Kontext ist eine Klasse, die von DbContext
+ * erbt, und das Arbeiten mit der Datenbank managt. Im Kontext kann der Entwickler das
+ * Laden von Objekten anstoßen und der Kontext "materialisiert" die Abfrageergebnisse
+ * zu Entitäts-Objekten. Der Kontext führt Buch über die Änderungen an diesen Objekten
+ * und sorgt dafür, dass die geänderten Objekte in der Datenbank gespeichert werden.
+ * 
+ * Das EF basiert auf ADO.NET (dem Nachfolger von ActiveX Data Objects), einer Daten-
+ * zugriffstechnologie, die einheitlich auf unterschiedliche Datenquellen zugreifen
+ * kann, wie z.B. SQL-Datenbanken, XML-Files oder Excel-Tabellen. Verschiedene Daten-
+ * provider regeln den Zugriff auf die verschiedenen Datenquellen.
+ * 
+ * Die in C# integrierte Abfragesprache LINQ (Language Integrated Query) trägt weiter
+ * zur Datenbankunabhängigkeit bei. Im EF kommt LINQ to Entities zum Einsatz, das
+ * automatisch in das sog. Entity SQL kompiliert wird und das der jeweilige Daten-
+ * provider in das native SQL der verwendeten Datenbank umwandelt.
+ * 
+ * Der Vorteil des Entity Frameworks besteht darin, dass der Entwickler, z.B. im
+ * Vergleich mit ADO.NET, viel weniger Datenzugriffscode (selbst) schreiben muss.
+ * 
+ * Das EF ist nicht geeignet für zeitkritische Anwendungen wie Massenaktualisierungen
+ * oder Echtzeit-Anwendungen.
+ */
+
 namespace EF_01_Grundlagen
 {
     class Program
@@ -14,13 +79,66 @@ namespace EF_01_Grundlagen
             // -----------------------------------------------
 
             // Als erstes ein Contextobjekt erstellen (in Model1.Context.cs zu finden)
-            // Zur using Anweisung gehört ein Codeblock, darum die {}
+            // Dazu wird ein using-Statement verwendet, damit offene Ressourcen (Datenbankverbindungen)
+            // am Ende des using-Blocks auf jeden Fall (auch im Fehlerfall) geschlossen werden.
             using (NorthwindEntities context = new NorthwindEntities()) 
-            { 
+            {
+                // Definition der Abfrage
+                // Die Variable products verweist auf die Auflistung Products (=DBSet<Product>) des
+                // Datenbankkontextes.
+                var products = context.Products;
+
+                Console.WriteLine("Alle Produkte:");
+
+                // Ausführen der Abfrage (erst jetzt fließen die Daten)
+                foreach (Product product in products)
+                {
+                    // Erst hier wird die connection zur Datenbank gemacht ↓↓↓
+                    Console.WriteLine($"{product.ProductID} - {product.ProductName,-40} {product.UnitPrice:n}");
+                    //Console.WriteLine($"{product.Order_Details.First().OrderID}");
+                }
+                Console.WriteLine();
 
             }
 
+            // LINQ to Entities Abfrage in Abfragesyntax
+            // -----------------------------------------
 
+            // Context erstellen
+            using (var ctx = new NorthwindEntities())
+            {
+                var productsMore80Euro = from p in ctx.Products
+                                         where p.UnitPrice > 80
+                                         select p;
+
+                Console.WriteLine("Alle Produkte die mehr als 80 Euro kosten:");
+                foreach (var product in productsMore80Euro)
+                {
+                    Console.WriteLine($"{product.ProductID} - {product.ProductName,-40} {product.UnitPrice:n} Euro");
+                }
+                Console.WriteLine();
+            }
+
+            // LINQ to Entities Abfrage im Methodensyntax
+            // ------------------------------------------
+
+            // Context erstellen
+            using(var ctx = new NorthwindEntities())
+            {
+                var orderDetails = ctx.Order_Details
+                    .Where(od => od.OrderID == ctx.Orders.Min(o => o.OrderID));
+
+                Console.WriteLine("Order mit der kleinsten OrderID:");
+                foreach (var item in orderDetails)
+                {
+                    //Console.WriteLine($"{item.OrderID, 6} {item.Quantity, 5} {item.ProductID, 5} {item.UnitPrice,8:n} Euro");
+
+                    // ODER Navigationseigenschaft "Product" verwenden, um den Produktnamen auszugeben
+
+                    Console.WriteLine($"{item.OrderID,6} {item.Quantity,5} {item.Product.ProductName, -40} {item.UnitPrice,8:n} Euro");
+                }
+                Console.WriteLine();
+            }
 
 
 
---
+++ EF_02_CUD/Program.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EF_02_CUD;
+
+namespace EF_02_LINQ2Entities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("\n***Beispiele zu LINQ2Entities***");
+
+                Console.WriteLine("1 - Alle Produkte auflisten");
+                Console.WriteLine("2 - Produkte in Flaschen die mehr als 10$ kosten");
+                Console.WriteLine("3 - Alle Bestellungen von Kunden aus Madrid");
+                Console.WriteLine("4 - Kunden nach Stadt gruppiert");
+                Console.WriteLine("5 - Bestellungen je Stadt mit Anzahl Bestellungen je Stadt");
+                Console.WriteLine("6 - Produkte mit bestimmten ProductIDs (mit Contains)");
+                Console.WriteLine("7 - Produkte japanischer Lieferanten, von denen weniger als 30 Einheiten lagernd sind");
+                Console.WriteLine();
+                Console.Write("      Auswahl:");
+                string auswahl = Console.ReadLine();
+
+                switch (auswahl)
+                {
+                    case "1": Option1(); break;
+                    case "2": Option2(); break;
+                    case "3": Option3(); break;
+                    case "4": Option4(); break;
+                    case "5": Option5(); break;
+                    case "6": Option6(); break;
+                    case "7": Option7(); break;
+                    default:
+                        return;
+                }
+                Console.ReadKey();
+            }
+        }
+
+        private static void Option7()
+        // Produkte japanischer Lieferanten, von denen weniger als 30 Einheiten lagernd sind
+        {
+            using (var context = new NorthwindEntities())
+            {
+                var query = from sup in context.Suppliers
+                            from prod in context.Products
+                            where sup.SupplierID == prod.SupplierID
+                            && sup.Country == "Japan"
+                            && prod.UnitsInStock < 30
+                            select new
+                            {
+                                prod.ProductID,
+                                prod.ProductName,
+                                prod.UnitsInStock,
+                                sup.CompanyName,
+                            };
+
+                Console.WriteLine("\nProdukte japanischer Lieferanten, von denen weniger als 30 Einheiten lagernd sind:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine($"{item.ProductID,3} {item.ProductName,-30}" +
+                        $" {item.CompanyName,-20} {item.UnitsInStock,4}");
+                }
+
+                Console.WriteLine($"\n{query.Count()} von insgesamt {context.Products.Count()} Produkten gefunden.");
+            }
+        }
+
+        private static void Option6()
+        // Produkte mit bestimmten ProductIDs (analaog zu SQL IN Klausel)
+        {
+            int[] productIds = { 22, 29, 69 };
+
+            using (var context = new NorthwindEntities())
+            {
+                var query = context.Products
+                    .Where(p => productIds.Contains(p.ProductID));
+
+                Console.WriteLine($"\nProdukte mit den IDs {string.Join(", ", productIds)}:");
+                foreach (var p in query)
+                {
+                    Console.WriteLine($"{p.ProductID} {p.ProductName}");
+                }
+            }
+
+        }
+
+        private static void Option5()
+        // Bestellungen (ID und Datum) je Stadt mit Anzahl der Bestellungen je Stadt.
+        // Am Ende der Städte noch die Anzahl der Städte, für die es Bestellungen gibt, ausgeben.
+        {
+            using (var context = new NorthwindEntities())
+            {
+                var queryOrder = from order in context.Orders
+                                 from cust in context.Customers
+                                 where order.CustomerID == cust.CustomerID
+                                 select new { order.OrderID, order.OrderDate, cust.City };
+
+                var cityGroupQuery = from order in queryOrder
+                                     group order by order.City into gr
+                                     select new
+                                     {
+                                         City = gr.Key,
+                                         Group = gr.ToList()
+                                     };
+
+                Console.WriteLine("\nBestellungen je Stadt mit Anzahl Bestellungen je Stadt:");
+                foreach (var g in cityGroupQuery)
+                {
+                    Console.WriteLine($"\n{g.City}, {g.Group.Count} Bestellung(en)");   // Gruppen-Header
+                    foreach (var o in g.Group)                                          // Schleife über alle Gruppenmember (Bestellungen)
+                    {
+                        Console.WriteLine($"    {o.OrderID} {o.OrderDate:d}");
+                    }
+                }
+                Console.WriteLine($"\nAnzahl Städte mit Bestellungen: {cityGroupQuery.Count()}");
+            }
+        }
+
+        private static void Option4()
+        // Kunden (Firmennamen) nach Stadt gruppiert, mit der Anzahl der Kunden je Stadt
+        // und der Anzahl der Städte (=Anzahl der Kundengruppen)
+        {
+            using (var context = new NorthwindEntities())
+            {
+                var query = from cust in context.Customers
+                            group cust by cust.City into gr
+                            select new
+                            {
+                                City = gr.Key,
+                                Group = gr.ToList()     // IGrouping<string, Customer> -> ListCustomer>
+                                                        // Mit der Liste kann man leichter weiterarbeiten
+                            };
+
+                Console.WriteLine("\nKunden (Firmennamen) nach Stadt gruppiert:");
+                foreach (var gr in query)
+                {
+                    Console.WriteLine($"\n{gr.City}({gr.Group.Count}:");      // Gruppen-Header
+                    foreach (var c in gr.Group)                         // Schleife über alle Gruppenmember (Kunden)
+                    {
+                        Console.WriteLine(c.CompanyName);
+                    }
+                }
+                Console.WriteLine($"\n{query.Count()} (Kundengruppen)");
+            }
+        }
+
+
+        // Hier werden, mittels join, zwei Tabellen abgefragt
+        private static void Option3()
+        // Alle Bestellungen von Kunden aus Madrid
+        {
+            using (var context = new NorthwindEntities())
+            {
+                var query = from order in context.Orders
+                            join cust in context.Customers
+                            on order.CustomerID equals cust.CustomerID
+                            where cust.City == "Madrid"
+                            select new
+                            {
+                                order.OrderID,
+                                order.OrderDate,
+                                cust.City,
+                                Company = cust.CompanyName
+                            };
+
+                Console.WriteLine("\nAlle Bestellungen von Kunden aus Madrid:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine($"{item.OrderID} {item.OrderDate} - {item.City} - {item.Company}");
+                }
+            }
+        }
+
+        public static void Option2()
+        // Produkte in Flaschen die mehr als 10$ kosten
+        {
+            using (var context = new NorthwindEntities())
+            {
+                var query = context.Products
+                    .Where(p => p.QuantityPerUnit.Contains("bottle") && p.UnitPrice > 10)
+                    .OrderByDescending(p => p.UnitPrice);
+
+                Console.WriteLine("\nProdukte in Flaschen die mehr als 10$ kosten:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine($"{item.ProductID,3} {item.ProductName,-35} " +
+                        $"{item.QuantityPerUnit,-20} {item.UnitPrice,8:n}");
+                }
+            }
+        }
+
+        public static void Option1()
+        // Alle Produkte mit Namen und ID auflisten
+        {
+            using (NorthwindEntities context = new NorthwindEntities())
+            {
+                // LINQ to Entities Query, um alle Produkte abzufragen (Methodensyntax)
+                var query = context.Products
+                    //.OrderBy(p => p.ProductID)
+                    .Select(x => new { x.ProductID, x.ProductName }) // ID und Name selektieren
+                    .OrderBy(p => p.ProductID);
+
+                Console.WriteLine("\nListe aller Produkte:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine($"{item.ProductID,3} {item.ProductName}");
+                }
+            }
+        }
+    }
+}
