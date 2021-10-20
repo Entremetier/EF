3b6e94af88037a9cb7e047f4e2638ae5c58d302e
Marco Happe <ITN247448@qualifizierung.at>
2021-10-20 17:34:24 +0200

    add stored procedures

---
+++ EF_03_CUD/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* EF erzeugt für jede Entität bei Bedarf automatisch eine SQL-Anweisung für die
+ * Operationen Create, Update und Delete (CUD) -> INSERT-, UPDATE-, DELETE-Anweisung.
+ * 
+ * Man kann aber auch SPs (Stored Procedure) für diese Operationen verwenden. 
+ * Dazu müssen die SPs mit dem
+ * EDM-Assistenten in das EDM eingebunden werden. Beim Import der SPs wird die Option
+ * "Ausgewählte gespeicherte Prozeduren und Funktionen in das Entitätsmodell
+ * importieren" deaktiviert, weil diese SPs der Entität direkt zugeordnet werden.
+ * 
+ * Nach dem Import kann man im Fenster "Zuordnungsdetails" über das 2. Icon auf der
+ * linken Seite (-> "Entität zu Funktionen zuordnen") festlegen, welche SP (Stored Procedure)
+ * für die Insert-, Update- und Delete-Operation verwendet werden soll. 
+ * Danach müssen noch den Parametern der SP Eigenschaften der Entität zugewiesen werden. 
+ * 
+ * Spalten, wie zum Beispiel fortlaufende IDs oder Zeitstempel, die im Zuge einer
+ * Insert- bzw. Update-Operation von der Datenbank gesetzt und in die Entität
+ * übernommen werden sollen, sind in der SP mittels einer SELECT-Anweisung zurück-
+ * zuliefern. Diesen Spalten müssen im Bereich "Bindungen für Ergebnisspalten"
+ * ebenfalls Eigenschaften der Entität zugewiesen werden.
+ * 
+ * Achtung: Wenn man für eine CUD-Operation eine SP (Stored Procedure) verwendet, 
+ * dann muss man für alle 3 CUD-Operationen SPs verwenden.
+ */
+
+namespace EF_03_CUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Alle Shipper ausgeben
+            Console.WriteLine("Alle Shipper:");
+            ShippersAusgeben();
+
+
+            // Neuen Shipper hinzufügen (CREATE)
+            Console.WriteLine("Neuen Shipper hinzufügen");
+            Console.WriteLine("------------------------");
+
+            using (var ctx = new NorthwindEntities())
+            {
+                // Ein neues Shippersobjekt erstellen
+                Shipper newShipper = new Shipper()
+                {
+                    CompanyName = "DHL",
+                    Phone = "(555)-147-2583"
+                };
+
+                // Neuen Shipper dem Change Tracker des Kontexts hinzufügen (State = "added")
+                ctx.Shippers.Add(newShipper);
+
+                // Die Änderungen am Kontext in der Datenbank speichern, erst hier passiert
+                // etwas in der Datenbank
+                ctx.SaveChanges();
+
+                Console.WriteLine($"\nNeue ShipperID: {newShipper.ShipperID}");
+                Console.WriteLine("\nAlle Shipper nach dem Hinzufügen eines neuen Shippers:");
+                ShippersAusgeben();
+            }
+
+
+            // Existierenden Shipper ändern (UPDATE)
+            Console.WriteLine("Existierenden Shipper ändern");
+            Console.WriteLine("----------------------------");
+
+            using (var ctx = new NorthwindEntities())
+            {
+                // Mittels Primärschlüssel eine Entität im Kontext finden
+                var shipper = ctx.Shippers.Find(4);
+
+                // Der Entität andere Eigenschaften zuweisen
+                shipper.CompanyName = "UPS";
+                shipper.Phone = "(123) 456-7890";
+
+                ctx.SaveChanges();  // Änderungen in der DB speichern
+
+                Console.WriteLine("\nAlle Shipper nach dem Ändern des Shippers mit der ID 4:");
+                ShippersAusgeben();
+            }
+
+
+
+            // Einen existierenden Shipper entfernen (DELETE)
+            Console.WriteLine("Einen Shipper löschen");
+            Console.WriteLine("---------------------");
+
+            using (var ctx = new NorthwindEntities())
+            {
+                // Den zuletzt hinzugefügten Shipper als Entität zurückliefern
+
+                //var shipper = ctx.Shippers.LastOrDefault(); // LINQ2Entity-Query (NotSupportedException
+                // weil LastOrDefault() von LINQ2Entitys
+                // nicht unterstützt werden, 
+                // Siehe: https://docs.microsoft.com/de-de/dotnet/framework/data/adonet/ef/language-reference/supported-and-unsupported-linq-methods-linq-to-entities
+
+
+                // Auch die nächste Anweisung führt zu einer NotSupportedException,
+                // weil die Sequenz vor dem Aufruf von Skip() unbedingt sortiert sein muss.
+                //var shipper = ctx.Shippers
+                //    .Skip(ctx.Shippers.Count() - 1)
+                //    .FirstOrDefault();
+
+
+                // Möglich wäre die folgende Anweisung, die aber u.U. nicht performant ist
+                // (bei sehr großen Tabellen, sortieren kann lange dauern).
+                //var shipper = ctx.Shippers
+                //    .OrderByDescending(s => s.ShipperID)
+                //    .FirstOrDefault();
+
+
+                //Lösung: SqlQuery() verwenden
+                var shipper = ctx.Shippers.SqlQuery("SELECT * FROM Shippers").LastOrDefault();
+
+                if (shipper != null)
+                {
+                    ctx.Shippers.Remove(shipper);   // Markiert die Entität als gelöscht
+
+                    ctx.SaveChanges();              // Änderungen in der DB speichern
+
+                    Console.WriteLine("\nAlle Shipper nach dem löschen des letzten Shippers:");
+                    ShippersAusgeben();
+                }
+
+
+            }
+
+
+
+            Console.ReadKey();
+        }
+
+        public static void ShippersAusgeben()
+        {
+            using (var context = new NorthwindEntities())
+            {
+                foreach (var s in context.Shippers)
+                {
+                    Console.WriteLine($"{s.ShipperID, -5} {s.CompanyName, -40} {s.Phone}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ EF_04_SP_Queries/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Stored Procedures zum Abrufen von Daten
+ * 
+ * Mit dem EF kann man nicht nur auf Tabellen zugreifen, sondern es können auch am Datenbank-
+ * Server gespeicherte Prozeduren zum Abfragen von Daten in das EDM eingebunden werden.
+ * Die SPs werden als Methoden in der Datenbankkontextklasse eingerichtet und können demnach
+ * über den Kontext aufgerufen werden.
+ * 
+ * Dazu müssen die SPs im EDM-Assistenten ausgewählt und die Option "Ausgewählte gespeicherte
+ * Prozeduren und Funktionen in das Entitätsmodell importieren" ausgewählt werden. Es wird
+ * für jede importierte SP ein sog. komlexer Typ eingerichtet, den das EF aus den Datentypen
+ * der Rückgabewerte ermittelt. Liefert die Abfrage eine Entität zurück, dann kann der
+ * komplexe Typ nachträglich durch die Entität ersetzt werden.
+ * 
+ * Hat man beim Import die o.g. Option nicht ausgewählt, dann kann man nachträglich im 
+ * Datenbankbereich des Modellbrowsers (...Model.Store) einzelne SPs als Methoden in das EDM
+ * einbinden (durch Auswahl der Option "Funktionsimport hinzufügen..." im Kontextmenü der
+ * jeweiligen SP).
+ */
+
+namespace EF_04_SP_Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Mit Stored Procedures Daten abrufen ***\n");
+
+            // Die 10 teuersten Produkte
+            // -------------------------
+
+            Console.WriteLine("Die 10 teuersten Produkte:");
+
+            using (var ctx = new NorthwindEntities())
+            {
+                var top10Products = ctx.Ten_Most_Expensive_Products();
+
+                // Das Resultat ausgeben
+                foreach (var item in top10Products)
+                {
+                    Console.WriteLine($"{item.TenMostExpensiveProducts, -25} {item.UnitPrice, 8:n} Euro");
+                }
+                Console.WriteLine();
+            }
+
+
+            // Die x teuersten Produkte
+            // -------------------------
+
+            using (var ctx = new NorthwindEntities())
+            {
+                int n = 15;
+
+                Console.WriteLine($"Die {n} teuersten Produkte:");
+
+                foreach (var item in ctx.sp_MostExpensiveProducts(n))
+                {
+                    Console.WriteLine($"{item.ProductID, -5} {item.ProductName, -35}" +
+                        $"{item.UnitPrice,8:n} Euro");
+                }
+                    
+            }
+
+
+
+            /* Übung: AlleBestellungenEinesKunden
+             *  
+             * Schreiben Sie eine Methode AusgebenAlleOrdersVonCustomer(), die eine Liste aller
+             * Bestellungen für einen Kunden auf der Konsole ausgibt. An die Methode soll eine
+             * CustomerID übergeben werden, die den Kunden angibt, für den die Bestellungen
+             * aufzulisten sind. Verwenden Sie in der Methode die (vorhandene) Stored Procedure
+             * "CustOrderHist" aus der Datenbank Northwind, um die Daten abzufragen. Rufen Sie
+             * die Methode einmal für den Kunden "ALFKI" und einmal für den Kunden "BOLID" auf.
+             * 
+             * Die Ausgabe sollte in etwa so aussehen:
+             * 
+             * Bestellungen des Kunden ALFKI:
+             *     6 Aniseed Syrup
+             *    21 Chartreuse verte
+             *    40 Escargots de Bourgogne
+             *    20 Flotemysost
+             *    16 Grandma's Boysenberry Spread
+             *    15 Lakkalikööri
+             *     2 Original Frankfurter grüne Soße
+             *    15 Raclette Courdavault
+             *    17 Rössle Sauerkraut
+             *     2 Spegesild
+             *    20 Vegie-spread
+             * 
+             * Bestellungen des Kunden BOLID:
+             *    40 Alice Mutton
+             *    24 Chef Anton's Cajun Seasoning
+             *    40 Filo Mix
+             *    16 Ravioli Angelo
+             *    50 Rhönbräu Klosterbier
+             *    20 Thüringer Rostbratwurst
+             * --------------------------------------------------------------------------------
+             */
+
+
+            Console.ReadKey();
+        }
+    }
+}
