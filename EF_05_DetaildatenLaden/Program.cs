489b72516f01bd4ed58125e4d8f438b700521d25
Marco Happe <ITN247448@qualifizierung.at>
2021-10-21 12:10:21 +0200

    add project 05 - Lazy Loading and project 06 - Eager Loading

---
+++ EF_05_DetaildatenLaden/Program.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/* Detaildaten, die über die Navigationseigenschaften zur Verfügung gestellt werden,
+ * können auf verschiedene Arten geladen werden:
+ *    - Lazy Loading      ("faules", verzögertes Laden)
+ *    - Explizites Laden
+ *    - Eager Loading     (vorzeitiges Laden)
+ *
+ * Beim Lazy Loading werden die Detaildaten automatisch abgerufen und zwar erst dann,
+ * wenn auf eine Navigationseigenschaft zugegriffen wird. Dabei wird ein SQL-Statement
+ * an den Server geschickt, um die zugehörigen Detaildaten abzufragen.
+ * 
+ * Standardmäßig ist im Entity Framework Lazy Loading eingeschaltet. Man kann das aber
+ * entweder für das gesamte Modell (im Eigenschaften-Fenster des EDM-Designers) oder
+ * auch nur für eine einzelne Kontext-Instanz (im Code) ändern.
+ * 
+ * Wenn man über lange Listen von Entitäten iteriert und dabei auf Detaildaten
+ * zugreift, dann sollte man Lazy Loading abschalten, damit nicht bei jeder Iteration
+ * eine Abfrage an den Server geschickt wird.
+ * 
+ * Wenn Lazy Loading abgeschaltet ist, dann werden die Detaildaten nicht mehr
+ * automatisch vom Entity Framework geladen, sondern man muss sich selbst darum
+ * kümmern. Diese Art des Ladens heißt Explizites Laden und es kommt dabei die Methode
+ * Load() zum Einsatz. Beim Expliziten Laden hat man zwar die Kontrolle über das Laden
+ * der Detaildaten, aber bei jedem Load()-Aufruf wird, wie beim Lazy Loading, eine
+ * SQL-Anweisung an den Server geschickt, um die Detaildaten abzufragen. Hinsichtlich
+ * der Performance ist das explizite Laden von Detaildaten in einer Schleife auch nicht
+ * besser als das Lazy Loading (u.U. sogar schlechter, wenn man nicht prüft, ob die
+ * Detaildaten bereits geladen wurden).
+ * 
+ * Im Entity Framework gibt es daher noch das sog. Eager Loading, bei dem man bereits
+ * beim Erstellen der Abfrage mittels der Include-Methode angeben kann, welche Detail-
+ * daten gleich mitgeladen werden sollen. Dabei verhindert man wiederholte Anfragen an
+ * den Server.
+ * 
+ * Beim Eager Loading werden aber alle Detaildaten geladen, die in der(n) Include-
+ * Methode(n) angegeben sind. Das kann zu sehr komplexen Queries und/oder zu großen
+ * Datenmengen führen, die vom DB-Server zum Client übertragen und danach noch
+ * materialisiert (in Objekte umgewandelt) werden müssen. Das ist vor allem dann
+ * problematisch, wenn bestimmte Detaildaten in weiterer Folge gar nicht verwendet
+ * werden. Es ist für jeden Einzelfall abzuwägen, ob Eager Loading oder Explizites
+ * Laden performanter bzw. resourcenschonender ist, oder ob eine Kombination aus beiden
+ * (Include und Load) günstiger ist (siehe dazu auch Projekt EF_06_EagerLoading).
+ */
+
+namespace EF_05_DetaildatenLaden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implizites Laden");
+            Console.WriteLine("----------------\n");
+
+            //TODO:
+            // Die Navigationseigenschaft ist eine Navigation Reference
+            // (Die Multiplizität der Assoziation zur anderen Entität ist 1 (oder 0).
+            // Siehe Model5.edmx (Diagram1) - Beziehung Category zu Product!!!!
+
+
+            Console.WriteLine("Produkte mit ID >= 75:");
+            using (var ctx = new NorthwindEntities())
+            {
+                var prods = ctx.Products.Where(p => p.ProductID >= 75);
+
+                Console.WriteLine("\n" + prods.ToString());     // SQL-Statement ausgeben
+
+                foreach (Product p in prods)
+                {
+                    Console.WriteLine($"{p.ProductID} - {p.ProductName}  (CategoryID = {p.CategoryID})");
+                    
+                    // Beim Zugriff auf die Navigationseigenschaft Category werden die zugehörigen
+                    // Detaildaten (z.B. CategoryName) automatisch (nach)geladen. Wenn der Name
+                    // schon einmal benutzt wurde bleibt er im Speicher und wird wiederverwendet.
+                    // Das bezeichnet man als "Lazy Loading", Standardmäßig ist es eingeschaltet.
+                    Console.WriteLine($"    {p.Category.CategoryName}");
+                }
+                Console.WriteLine();
+            }
+
+
+            //TODO:
+            // Die Navigationseigenschaft ist eine Navigation Collection
+            // (Die Multiplizität der Assoziation zur anderen Entität ist * (many).
+            // Siehe Model5.edmx (Diagram1) - Beziehung Product zu Category !!!!
+
+
+            Console.WriteLine("Kategorien 6 und 7:");
+            using (var ctx = new NorthwindEntities())
+            {
+                var cats = ctx.Categories.Where(c => c.CategoryID >= 6 && c.CategoryID <= 7);
+
+                Console.WriteLine("\n" + cats.ToString());     // SQL-Statement ausgeben
+
+                foreach (Category c in cats)
+                {
+
+                    // Die zur Kategorie c gehörende Produkteliste wird bei eingeschaltetem
+                    // Lazy Loading beim Zugriff auf Produktdaten (über die Navigationseigenschaft
+                    // "Products") automatisch nachgeladen.
+                    Console.WriteLine($"\n{c.CategoryName} ({c.Products.Count()} Produkte)");
+
+                    foreach (var p in c.Products)
+                    {
+                    Console.WriteLine($"    {p.ProductName}");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+
+
+            Console.WriteLine("\nExplizites Laden");
+            Console.WriteLine("----------------\n");
+
+
+            // Navigation Reference
+
+            // Um Lazy Loading für das Projekt auszuschalten geht man in Model5.edmx, rechtsklick
+            // auf einen freien Bereich und Properties aufrufen, dann "Lazy Loading Enabled" auf
+            // false setzen. 
+
+            Console.WriteLine("Produkte mit ID >= 75:");
+            using (var ctx = new NorthwindEntities())
+            {
+                // Lazy Loading nur für diesen Kontext (nicht das ganze Projekt) abschalten
+                // Das automatische Laden von Detaildaten ist damit ausgeschaltet.
+                ctx.Configuration.LazyLoadingEnabled = false;
+
+                var prods = ctx.Products.Where(p => p.ProductID >= 75);
+
+                foreach (Product p in prods)
+                {
+                    Console.WriteLine($"{p.ProductID} - {p.ProductName}  (CategoryID = {p.CategoryID})");
+
+                    // Wenn Lazy Loading ausgeschaltet ist, dann wird beim Zugriff auf die
+                    // Navigationseigenschaft "Category" eine NullReferenceException geworfen,
+                    // da die zugehörigen Detaildaten nicht geladen sind.
+                    // Die Navigationseigenschaft "Category" = null.
+
+                    // Die zur Navigationseigenschaft "Category" gehörenden Detaildaten können
+                    // explizit geladen werden. Da ein Produkt zu einer (oder keiner) Kategorie
+                    // gehört, werden die Kategoriedaten mit den Methoden Reference() und Load()
+                    // geladen.
+                    // Vor dem Laden sollte geprüft werden, ob die Daten nicht bereits geladen
+                    // wurden.
+
+
+                    // Wenn die Navigationseigenschaft noch nicht geladen ist, wird sie (nach)geladen
+                    //if (!ctx.Entry(p).Reference("Category").IsLoaded)
+                    //{
+                    //// Durch Entry kann das einzelne Product angesprochen werden
+                    //ctx.Entry(p).Reference("Category").Load();
+                    //}
+
+
+                    // ODER mit Lambda-Ausdruck (und IntelliSense-Unterstützung)
+
+
+                    if (!ctx.Entry(p).Reference(c => c.Category).IsLoaded)
+                    {
+                        ctx.Entry(p).Reference(c => c.Category).Load();
+                    }
+
+                    // Die Navigationseigenschaft "Category" könnte trotz explizitem Laden null
+                    // sein, wenn dem Produkt in der DB keine Kategorie zugeordnet ist.
+                    // (Das Feld "CategoryID" der Tabelle "Product" erlaubt Nullwerte!)
+                    if (p.Category != null)
+                    {
+                    Console.WriteLine($"    {p.Category.CategoryName}");
+
+                    }
+                }
+                Console.WriteLine();
+            }
+
+
+            // Navigation Collection
+
+            Console.WriteLine("Kategorien 6 und 7:");
+            using (var ctx = new NorthwindEntities())
+            {
+                ctx.Configuration.LazyLoadingEnabled = false;
+
+                var cats = ctx.Categories.Where(c => c.CategoryID >= 6 && c.CategoryID <= 7);
+
+                foreach (Category cat in cats)
+                {
+                    // Wenn Lazy Loading ausgeschaltet ist, dann werden die zur Navigations-
+                    // eigenschaft "Products" gehörigen Detaildaten (= Liste aller zur Kategorie
+                    // gehörenden Produkte) nicht geladen und die Liste ist leer.
+
+                    // Die zur Navigationseigenschaft "Products" gehörenden Detaildaten können
+                    // explizit geladen werden. Da zu einer Kategorie viele Produkte gehören,
+                    // werden die Produktdaten mit den Methoden Collection() und Load() geladen.
+                    // Vor dem Laden sollte geprüft werden, ob die Daten nicht bereits geladen
+                    // wurden.
+
+                    if (!ctx.Entry(cat).Collection("Products").IsLoaded)
+                    {
+                        ctx.Entry(cat).Collection(c => c.Products).Load();
+                    }
+
+                    Console.WriteLine($"\n{cat.CategoryName} ({cat.Products.Count()} Produkte)");
+
+                    foreach (var p in cat.Products)
+                    {
+                        Console.WriteLine($"    {p.ProductName}");
+                    }
+                }
+            }
+
+
+            Console.WriteLine("\nEager Loading");
+            Console.WriteLine("---------------");
+
+            // Navigation Reference
+
+            using (var ctx = new NorthwindEntities())
+            {
+                var prods = ctx.Products
+                    .Include("Category")        // Detaildatensätze aus Category werden hier schon mitgeladen
+                    .Where(p => p.ProductID >= 75);
+
+                Console.WriteLine("\n" + prods.ToString());     //SQL-Statement ausgeben
+
+                foreach (Product p in prods)
+                {
+                    Console.WriteLine($"\n{p.ProductID} - {p.ProductName}  (CategoryID = {p.CategoryID})");
+                    Console.WriteLine($"    {p.Category.CategoryName}");
+                }
+                Console.WriteLine();
+            }
+
+
+            // Navigation Collection
+
+            using (var ctx = new NorthwindEntities())
+            {
+                var cats = ctx.Categories
+                    .Include("Products")
+                    .Where(c => c.CategoryID >= 6 && c.CategoryID <= 7);
+
+                Console.WriteLine("\n" + cats.ToString());     //SQL-Statement ausgeben
+
+                foreach (Category c in cats)
+                {
+                    Console.WriteLine($"\n{c.CategoryName} ({c.Products.Count()} Produkte)");
+
+                    foreach (var p in c.Products)
+                    {
+                        Console.WriteLine($"    {p.ProductName}");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EF_06_EagerLoading/Program.cs
@@ -0,0 +1,288 @@
+﻿/* Im Entity Framework kann man mittels Eager Loading bereits beim Erstellen einer
+ * Abfrage angeben, welche Detaildatensätze gleich mitgeladen werden sollen. In der
+ * Abfrage wird dazu die Include-Methode verwendet. Dabei verhindert man wiederholte
+ * Anfragen an den Server wie das beim Lazy Loading der Fall ist.
+ * 
+ * Mit der Include-Methode können die Detaildatensätze mehrerer Ebenen von Navigations-
+ * eigenschaften auf einmal gelesen werden. Das Entity Framework generiert ein SQL-
+ * Statement mit den zugrundeliegenden Tabellen und den entsprechenden Joins.
+ * 
+ * Standardmäßig kann die Include-Methode nur mit einem String-Parameter verwendet
+ * werden, der den Namen einer Navigationseigenschaft angibt. Um statt mit dem String-
+ * Parameter mit einem Lambda-Ausdruck arbeiten zu können, muss der Namespace
+ * 
+ *    System.Data.Entity
+ *    
+ * eingebunden werden. Dieser enthält die Klasse QueryableExtensions, die eine Reihe
+ * von Erweiterungsmethoden für Entity Framework LINQ Abfragen zur Verfügung stellt,
+ * siehe MSDN QueryableExtensions Class. Die Include-Methode mit Lambda-Ausdruck als
+ * Parameter hat den Vorteil, dass man IntelliSense-Unterstützung hat, was beim String-
+ * Parameter nicht der Fall ist.
+ * 
+ * Bei Referenzdetaildaten (Navigationseigenschaft zu einer einzelnen anderen Entität)
+ * können die Eigenschaften der Detaildaten überall in der Query ohne Einschränkung
+ * verwendet werden.
+ * 
+ * Bei Detaildatenlisten (Navigationseigenschaft zu einer Collection anderer Entitäten)
+ * kann man die Eigenschaften der Detaildaten zwar in der Query verwenden, allerdings
+ * nur mit Methoden, die sich auf eine Collection beziehen, wie z.B. die Aggregations-
+ * Operatoren (Count, Min, Max, ...) oder die Quantifizierungs-Operatoren (Any, All,
+ * Contains). Die Detaildatenlisten können aber nicht gefiltert oder sortiert werden.
+ * Das ist nur mittels Projektion oder Umdrehen der Query möglich.
+ */
+
+using System;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;     // Namespace der Klasse QueryableExtensions einbinden
+
+namespace EF_06_EagerLoading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // -> Tabellen "Employees", "EmployeeTerritories", "Territories" und "Region"
+            //    importieren
+            // -> Die Verbindungstabelle "EmployeeTerritories" wird nicht in das EDM
+            //    aufgenommen. Stattdessen gibt es die Navigationseigenschaften
+            //    "Employee.Territories" und "Territory.Employees".
+
+            Console.WriteLine("\nEager Loading - Include mit Lambda-Ausdruck");
+            Console.WriteLine("-------------------------------------------");
+
+            using (NorthwindEntities ctx = new NorthwindEntities())
+            {
+                // Ohne "using System.Data.Entity;" kann als Parameter für die Include-Methode
+                // nur der Name einer Navigationseigenschaft (string), aber kein Lambda-Ausdruck
+                // verwendet werden.
+                // -> Include() kann auch nicht hinter Where() verwendet werden!
+
+                var query = ctx.Territories.Include(t => t.Region);
+
+                Console.WriteLine("\n" + query.ToString());  // SQL-Statement ausgeben
+
+                Console.WriteLine("\nTerritorien mit zugehöriger Region:\n");
+                foreach (Territory t in query)
+                    Console.WriteLine($"{t.TerritoryID} {t.TerritoryDescription} {t.RegionID} {t.Region.RegionDescription}");
+            }
+
+
+            Console.WriteLine("\nLazy Loading - 2 Ebenen");
+            Console.WriteLine("-----------------------");
+
+            using (NorthwindEntities ctx = new NorthwindEntities())
+            {
+                var query = ctx.Employees;
+
+                Console.WriteLine("\nAngestellte mit zugehörigen Territorien und Regionen:\n");
+                foreach (Employee e in query)
+                {
+                    Console.WriteLine($"{e.EmployeeID} {e.FirstName + " " + e.LastName}");
+
+                    // Die Detaildaten werden beim Zugriff auf die Navigationseigenschaften
+                    // mit separaten SQL Statements nachgeladen.
+                    foreach (Territory t in e.Territories)
+                        Console.WriteLine($"   {t.TerritoryDescription} {t.Region.RegionDescription}");
+                }
+            }
+
+            Console.WriteLine("\nEager Loading - 2 Ebenen");
+            Console.WriteLine("------------------------");
+
+            using (NorthwindEntities ctx = new NorthwindEntities())
+            {
+                // Die Detaildatensätze der Navigationseigenschaften "Territories" und "Region"
+                // gleich mit den "Employees"-Daten laden.
+                var query = ctx.Employees
+                   .Include("Territories.Region")   // Ebenen voneinander durch Punkte trennen!
+                                                    // ODER Include() mit Lambda-Ausdruck
+                   .Include(e => e.Territories.Select(r => r.Region));
+
+                // Allgemein gilt für Include() mit Lambda-Ausdruck als Parameter:
+                // Nach Referenz-Navigationseigenschaften kommt man mit . zur nächsten Ebene,
+                // nach Collection-Navigationseigenschaften muss man .Select() verwenden.
+                // z.B.: ctx.Products.Include(p => p.Order_Details.Select(od => od.Order.Employee.Territories.Select(t => t.Region)));
+
+                Console.WriteLine("\n" + query.ToString());
+
+                // -> Ausgabe wie im vorigen Beispiel (Lazy Loading)
+                Console.WriteLine("\nAngestellte mit zugehörigen Territorien und Regionen:\n");
+                foreach (Employee e in query)
+                {
+                    Console.WriteLine($"{e.EmployeeID} {e.FirstName + " " + e.LastName}");
+
+                    foreach (Territory t in e.Territories)
+                        Console.WriteLine($"   {t.TerritoryDescription} {t.Region.RegionDescription}");
+                }
+            }
+
+
+            Console.WriteLine("\nEager Loading mit Bedingung");
+            Console.WriteLine("Navigationseigenschaft zu einer einzelnen anderen Entität");
+            Console.WriteLine("---------------------------------------------------------");
+
+            using (NorthwindEntities ctx = new NorthwindEntities())
+            {
+                var query = ctx.Territories
+                   .Include(t => t.Region)
+                   .Where(t => t.Region.RegionDescription == "Northern");
+
+                Console.WriteLine("\n" + query.ToString());
+
+                Console.WriteLine("\nTerritorien mit Region \"Northern\":\n");
+                foreach (Territory t in query)
+                {
+                    Console.WriteLine($"{t.TerritoryID} {t.TerritoryDescription} {t.RegionID} {t.Region.RegionDescription}");
+                }
+            }
+
+            Console.WriteLine("\nEager Loading mit Bedingung");
+            Console.WriteLine("Navigationseigenschaft zu einer Collection anderer Entitäten");
+            Console.WriteLine("------------------------------------------------------------");
+
+            using (NorthwindEntities ctx = new NorthwindEntities())
+            {
+                var query = ctx.Regions
+                   .Include(r => r.Territories)
+                   .Where(r => r.Territories.Count() < 12);
+                // Bedingung, die sich auf die gesamte Collection bezieht (-> Count)
+
+                Console.WriteLine("\n" + query.ToString());
+
+                Console.WriteLine("\nRegionen mit weniger als 12 Territorien:\n");
+                foreach (Region r in query)
+                {
+                    Console.WriteLine($"Region {r.RegionID} {r.RegionDescription.Trim()} ({r.Territories.Count()} Territorien)");
+                    foreach (Territory t in r.Territories)
+                        Console.WriteLine($"   {t.TerritoryID} {t.TerritoryDescription}");
+                }
+            }
+
+            using (NorthwindEntities ctx = new NorthwindEntities())
+            {
+                var query = ctx.Regions
+                   .Include(r => r.Territories)
+                   .Where(r => r.Territories.Any(t => t.TerritoryDescription.StartsWith("A")));
+                // Bedingung mit einer Eigenschaft der Detaildatenliste. Die Eigenschaft der
+                // Collection kann aber nur in einer Methode verwendet werden, die sich auf die
+                // gesamte Collection bezieht (hier z.B. Any).
+
+                Console.WriteLine("\n" + query.ToString());
+
+                Console.WriteLine("\nRegionen mit zumindest einem Territorium, das mit \"A\" beginnt:\n");
+                foreach (Region r in query)
+                {
+                    Console.WriteLine($"Region {r.RegionID} {r.RegionDescription.Trim()} ({r.Territories.Count()} Territorien)");
+                    foreach (Territory t in r.Territories)
+                    {
+                        Console.WriteLine($"   {t.TerritoryID} {t.TerritoryDescription}");
+                    }
+                }
+
+                // Achtung:
+                // Any prüft, ob es unter den Territorien zumindest 1 Element gibt, das mit "A"
+                // beginnt. Wenn das zutrifft, dann wird die Region in das Resultset aufgenommen.
+                // Für diese Region werden aber ALLE zugehörigen Territorien geladen!
+
+                // Man kann die Eigenschaften der inkludierten Daten in der Query verwenden, man
+                // kann aber nicht die inkludierten Daten filtern oder sortieren!
+
+                //var query1 = ctx.Regions
+                //   .Include(r => r.Territories)
+                //   .Where(r => r.Territories.TerritoryDescription.StartsWith("A"));
+                //
+                // führt zu einem Compiler-Fehler.
+            }
+
+
+            Console.WriteLine("\nInkludierte Detaildatenlisten mittels Projektion filtern");
+            Console.WriteLine("--------------------------------------------------------");
+
+            using (NorthwindEntities ctx = new NorthwindEntities())
+            {
+                var query = ctx.Regions
+                   .Include(r => r.Territories)
+                   // Es ist egal, ob mit oder ohne Include(), die SQLs sind identisch!
+                   .Select(region => new
+                   {
+                       region,
+                       Ters = region.Territories.Where(t => t.TerritoryDescription.Trim().EndsWith("s"))
+                   });
+                // region.Territories enthält alle Territorien, Ters nur jene, die auf "s" enden
+
+                Console.WriteLine("\n" + query.ToString());
+
+                Console.WriteLine("\nAlle Regionen mit allen Territorien:\n");
+                foreach (var item in query)
+                {
+                    Console.WriteLine($"Region {item.region.RegionID} {item.region.RegionDescription}");
+                    foreach (Territory t in item.region.Territories)
+                    {
+                        Console.WriteLine($"   {t.TerritoryID} {t.TerritoryDescription}");
+                    }
+                }
+
+                Console.WriteLine("\nRegionen mit Territorien, die auf \"s\" enden:\n");
+                foreach (var item in query)
+                {
+                    if (item.Ters.Count() > 0)
+                        Console.WriteLine($"Region {item.region.RegionID} {item.region.RegionDescription}");
+                    foreach (Territory t in item.Ters)
+                    {
+                        Console.WriteLine($"   {t.TerritoryID} {t.TerritoryDescription}");
+                    }
+                }
+            }
+
+            Console.WriteLine("\nInkludierte Detaildaten durch Umdrehen der Query filtern");
+            Console.WriteLine("--------------------------------------------------------");
+
+            // Query umdrehen: Von den Territorien ausgehen -> man bekommt ein flaches Resultat.
+
+            using (NorthwindEntities ctx = new NorthwindEntities())
+            {
+                var query = ctx.Territories
+                   .Include(t => t.Region)          // ohne Include() -> Lazy Loading!
+                   .Where(t => t.TerritoryDescription.TrimEnd().EndsWith("s"))
+                   .OrderBy(t => t.Region.RegionID);      // -> erst in einem 2. Schritt
+
+                Console.WriteLine("\n" + query.ToString());
+
+                Console.WriteLine("\nTerritorien, die auf \"s\" enden mit zugehöriger Region:\n");
+                foreach (Territory t in query)
+                {
+                    Console.WriteLine($"Territorium {t.TerritoryID} {t.TerritoryDescription} Region {t.Region.RegionID} {t.Region.RegionDescription} ");
+                }
+            }
+
+
+            // ToDo -> Ev. Vergleich mit Join
+
+            // Join
+
+            //   var cats2 = ctx.Categories.Join(ctx.Products,
+            //      c => c.CategoryID,
+            //      p => p.CategoryID,
+            //      (c, p) => new
+            //      {
+            //         c.CategoryID,
+            //         c.CategoryName,
+            //         p.ProductID,
+            //         p.ProductName
+            //      })
+            //      ;
+
+            //   Console.WriteLine("\n" + cats2.ToString());
+
+            // Projektion
+
+            //   var cats3 = ctx.Categories
+            //      .Select(c => new { c, c.Products });
+
+            //   Console.WriteLine("\n" + cats3.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
